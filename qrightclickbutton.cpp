960ec9d33fb4c7061423a1d7568c82c83b1ea129
Lisk0 <nemanjalisinac@gmail.com>
2019-12-26 01:57:35 +0100

    dodati komentari u klasi

--- qrightclickbutton.cpp
+++ qrightclickbutton.cpp
@@ -1,10 +1,14 @@
 #include "qrightclickbutton.h"
 
+//klasa koja nasledjuje QPushButton
+//jedina razlika je drugacije implementiran mousePressEvent
 QRightClickButton::QRightClickButton(QWidget *parent) :
     QPushButton(parent)
 {
 }
 
+//override mousePressEvent tako da salje razlicite signale
+//pritiskom na levi ili desni klik
 void QRightClickButton::mousePressEvent(QMouseEvent *e)
 {
     if(e->button()==Qt::RightButton)
